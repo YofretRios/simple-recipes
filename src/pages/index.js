66528181bde8dc88a7b208935281eac257b4694c
Yofret Rios <yofret.rios@firstleaf.com>
2021-04-05 15:36:31 -0500

    feat: starting with home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/shared/Layout';
-import * as styles from '../styles/home.module.scss';
 
 const Index = () => {
   return (
     <Layout>
-      <StaticImage
-        src="../assets/images/recipes/recipe-1.jpeg"
-        alt="test image"
-        className={styles.staticImg}
-        height={400}
-        placeholder="tracedSVG"
-      />
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Simply Recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   );
 };
